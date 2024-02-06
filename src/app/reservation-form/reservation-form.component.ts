d581985d032a0079709431bcb6ba279c3f2617a6
Chris Allen <christopherallen777@gmail.com>
2024-02-06 17:33:11 -0600

    adding a router redirect

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../model/reservation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -17,7 +18,8 @@ export class ReservationFormComponent implements OnInit{
   // Once injected into a constructor you can use in the entire component
   constructor(
     private formBuilder: FormBuilder,
-    private reservationService: ReservationService) {
+    private reservationService: ReservationService,
+    private router: Router) {
 
   }
 
@@ -36,6 +38,8 @@ export class ReservationFormComponent implements OnInit{
 
       let reservation: Reservation = this.reservationForm.value;
       this.reservationService.addReservation(reservation);
+
+      this.router.navigate(['/list']);
     }
   }
 
